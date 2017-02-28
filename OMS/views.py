040ca9874520bac3cc63b3baf8e0f23216e30e66
Luke Higgott <higgottluke@gmail.com>
2017-02-27 21:14:17 -0800

    removed reportlab thing

--- OMS/views.py
+++ OMS/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from reportlab.pdfgen import canvas
 from django.http import HttpResponse
 from .models import Patient, Audiogram, Appointment
 from django.contrib.auth.decorators import login_required
