2bd69998ee7ad92d263ed8231a73c015b575b6ea
Luke Higgott <higgottluke@gmail.com>
2017-03-01 12:33:56 -0800

    patient form autocomplete disabled

--- OMS/forms.py
+++ OMS/forms.py
@@ -4,17 +4,20 @@ from .models import Patient
 
 class PatientForm(forms.ModelForm):
     class Meta:
+
+        standardreq = {'class':'form-control', 'required':"", 'autocomplete':"off"}
+        standard = {'class':'form-control', 'autocomplete':"off"}
         model = Patient
         fields = ['fname', 'lname', 'gender', 'birthday', 'phone', 'email', 'street', 'city', 'state', 'zipcode', 'notes']
         widgets = {
-            'fname' : TextInput(attrs={'class':'form-control', 'required':""}),
-            'lname' : TextInput(attrs={'class':'form-control', 'required':""}),
-            'gender': Select(attrs={'class': 'form-control', 'required':""}),
-            'birthday': TextInput(attrs={'class': 'form-control', 'required':""}),
-            'email': EmailInput(attrs={'class':'form-control'}),
-            'street': TextInput(attrs={'class': 'form-control'}),
+            'fname' : TextInput(attrs=standardreq),
+            'lname' : TextInput(attrs=standardreq),
+            'gender': Select(attrs=standardreq),
+            'birthday': TextInput(attrs=standard),
+            'email': EmailInput(attrs=standard),
+            'street': TextInput(attrs=standard),
             'phone': TextInput(attrs={'class': 'form-control', 'number':'true', 'minlength':'10', 'maxlength':'10'}),
-            'city': TextInput(attrs={'class': 'form-control'}),
+            'city': TextInput(attrs=standard),
             'state': TextInput(attrs={'class': 'form-control', 'minlength':'2', 'maxlength':'2'}),
             'zipcode': TextInput(attrs={'class': 'form-control', 'minlength':'5', 'maxlength':'5', 'number':'true'}),
             'notes': Textarea(attrs={'class': 'form-control autoExpand', 'rows':'2', 'data-min-rows':'2'}),
